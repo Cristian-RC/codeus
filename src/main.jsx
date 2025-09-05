89cbf4fccffb3ff1a081a4bbe8bb930d3a62ad36
Cristian Rubio <cristian.rubio@daksha.mx>
2025-09-05 15:16:24 -0600

    🔥 Remove unused index.scss import from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 import './styles/tailwind.css';
-import './styles/index.scss';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
