4a0309b20ba7610aaec16cf6cf6f093b9a28b049
Cristian Rubio <cristian.rubio@daksha.mx>
2025-12-11 13:19:09 -0600

    🎨 Update certs.js: replace placeholder certificates with actual credentials and images for enhanced portfolio presentation

--- src/data/certs.js
+++ src/data/certs.js
@@ -1,6 +1,9 @@
+import VertexBadge from "@assets/images/certs/Build_Deploy_ML_Credly.png";
+import DataAnalyst from "@assets/images/certs/Data_Analiyst_Credly.png";
+import PromptVertex from "@assets/images/certs/Prompt_Vertex.png"
+
 export const certs = [
-  { name: "Google Cloud — Vertex AI", org: "Google Cloud", year: "2025", img: "", href: "#" },
-  { name: "Machine Learning Scientist", org: "DataCamp", year: "2024", img: "", href: "#" },
-  { name: "FastAPI Profesional", org: "Platzi", year: "2024", img: "", href: "#" },
-  { name: "React + Next.js", org: "Udemy", year: "2023", img: "", href: "#" },
+  { name: "Build and Deploy Machine Learning Solutions on Vertex AI", org: "Google Cloud", year: "2024", img: VertexBadge, href: "https://www.credly.com/badges/cf072e15-98ed-4929-a8b0-013034f8c3c5/public_url" },
+  { name: "Certificado de análisis de datos de Google", org: "Coursera + Google", year: "2024", img: DataAnalyst, href: "https://www.credly.com/badges/aca42922-25a5-49fb-b5d4-d7ec40e29b07/public_url" },
+  { name: "Prompt Design in Vertex AI", org: "Google Cloud", year: "2024", img: PromptVertex, href: "https://www.credly.com/badges/3c95e3aa-aeb9-4327-82e0-4e3dada39257/public_url" },
 ];
